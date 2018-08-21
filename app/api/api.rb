97fdce61df9095b20acdb67b0cd1cd7e093b8f6c
Peter M Adrianov <peter.adrianov@gmail.com>
2018-08-21 15:09:22 +0300

    Add Products API.

--- app/api/api.rb
+++ app/api/api.rb
@@ -2,4 +2,11 @@
 class Api < Grape::API
   format :json
   # rescue_from ActiveRecord::RecordInvalid, ActiveRecord::RecordNotFound
+
+  resources :products do
+    # Get all Products
+    get do
+      present :data, Product.all
+    end
+  end
 end
